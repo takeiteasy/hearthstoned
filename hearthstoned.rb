61f2ed2055abcc1c17dbf26ab0eb3f0e8a27f976
George Watson <gigolo@hotmail.co.uk>
2023-03-03 11:32:45 +0000

    handling more log outputs

--- hearthstoned.rb
+++ hearthstoned.rb
@@ -1,6 +1,12 @@
 require 'json'
 require 'singleton'
 
+def Error(msg, die=false)
+    puts "#{die ? "FATAL " : ""}ERROR: #{msg}"
+    puts "##{State.instance.currentLineNo}: #{State.instance.currentLine}"
+    abort if die
+end
+
 class State 
     include Singleton
     attr_accessor :inGame, :entities, :lastEntity, :currentLine, :currentLineNo
@@ -22,7 +28,7 @@ class State
     end
 
     def addEntity(id)
-        Die("Entity already exists with ID: #{id}") if hasEntity? id 
+        return if hasEntity? id 
         @lastEntity = id
         @entities[id] = {}
     end
@@ -32,7 +38,7 @@ class State
     end
 
     def setEntityTag(id, key, value)
-        Die("Entity already exists with ID: #{id}") unless hasEntity? id
+        return unless hasEntity? id
         @entities[id][key] = value
     end
 
@@ -46,18 +52,11 @@ class State
     end
 end
 
-def Die(msg)
-    puts "FATAL ERROR: #{msg}"
-    puts "##{State.instance.currentLineNo}: #{State.instance.currentLine}"
-    abort
-end
-
 def ParseGameState(type, tags)
     case type
     when "DebugPrintPower"
         case tags
         when "CREATE_GAME"
-            State.instance.reset true
         when /^GameEntity EntityID=(\d+)$/
             State.instance.addEntity $1
         when /^Player EntityID=(\d+) PlayerID=(\d+) GameAccountId=\[hi=(\d+) lo=(\d+)\]$/
@@ -70,30 +69,85 @@ def ParseGameState(type, tags)
             State.instance.setEntityTag $1, "CardID", $2 unless $2.empty? or $2.nil?
         when /^tag=(\S+) value=(\S+)$/
             State.instance.setEntityTag State.instance.lastEntity, $1, $2
-        when /^TAG_CHANGE Entity=(.*) tag=(\S+) value=(\S+)$/
-            entity = $1
-            key = $2
-            value = $3
+        when /^\s*TAG_CHANGE Entity=(.*) tag=(\S+) value=(\S+)( DEF CHANGE)?$/
+            entity  = $1
+            key     = $2
+            value   = $3
             case entity
             when /^\d+$/
                 State.instance.setEntityTag entity, key, value
             when /^\[/
-                if entity =~ /\[entityName=(.*) (\[cardType=(\S+)\] )?id=(\d+) zone=(\S+) zonePos=(\d+) cardId=(\S+)? player=(\d+)\]/
+                if entity =~ /^\[entityName=(.*) id=(\d+) zone=(\S+) zonePos=(\d+) cardId=(\S+)? player=(\d+)\]$/
+                    entityName = $1
+                    id         = $2
+                    zone       = $3
+                    zonePos    = $4
+                    cardId     = $5
+                    player     = $6
+                    if entityName =~ /^UNKNOWN ENTITY \[cardType=(\S+)\]$/
+                        entityName = "UNKNOWN ENTITY"
+                        State.instance.setEntityTag id, "cardType", $1
+                    end
+                    State.instance.setEntityTag id, "entityName", entityName
+                    State.instance.setEntityTag id, "ZONE", zone
+                    State.instance.setEntityTag id, "ZONE_POSITION", zonePos
+                    State.instance.setEntityTag id, "CardID", cardId unless cardId.nil?
+                    State.instance.setEntityTag id, "player", player
                 else
-                    Die("Unhandled TAG_CHANGE format")
+                    Error "Unhandled TAG_CHANGE format"
                 end
             when /GameEntity/
                 State.instance.setEntityTag "1", key, value
             else
                 State.instance.players.each do |k, v|
                     if v.key? "PlayerName" and v["PlayerName"] == entity
-                        State.instance.setEntityTag k, key, value
+                        v[key] = value
                         break
                     end
                 end
             end
+        when /^SHOW_ENTITY - Updating Entity=(.*) CardID=(\S+)$/
+            entity = $1
+            cardid = $2
+            case entity
+            when /^\d+$/
+                State.instance.setEntityTag entity, "CardID", cardid
+            when /^\[entityName=(.+) id=(\d+) zone=(\S+) zonePos=(\d+) cardId=(\S+)? player=(\d+)\]$/
+                entityName = $1
+                id         = $2
+                zone       = $3
+                zonePos    = $4
+                cardId     = $5
+                player     = $6
+                if entityName =~ /^(.+)\[cardType=(\S+)\]$/
+                    entityName = $1.rstrip
+                    State.instance.setEntityTag id, "cardType", $2
+                end
+                State.instance.setEntityTag id, "entityName", entityName
+                State.instance.setEntityTag id, "ZONE", zone
+                State.instance.setEntityTag id, "ZONE_POSITION", zonePos
+                State.instance.setEntityTag id, "CardID", cardId unless cardId.nil?
+                State.instance.setEntityTag id, "player", player
+            else
+                Error "Unhandled SHOW_ENTITY format"
+            end
+        when /^HIDE_ENTITY - Entity=(.*) tag=(\S+) value=(\S+)$/
+            # TODO
+        when /^CHANGE_ENTITY - Updating Entity=(\S+) CardID=(\S+)$/
+            # TODO
+        when /^BLOCK_START/
+        when "BLOCK_END"
+        when /^CACHED_TAG_FOR_DORMANT_CHANGE Entity=(.*) tag=(\S+) value=(\S+)$/
+            # TODO
+        when /^META_DATA - Meta=(\S+) Data=(\d+) InfoCount=(\d+)$/
+        when /^Info\[(\d+)\] = (.*)$/
+        when /^Source = (.*)$/
+        when /^Targets\[(\d+)\] = (.*)$/
+        when /^SHUFFLE_DECK PlayerID=(\d+)$/
+        when /^SUB_SPELL_START/
+        when "SUB_SPELL_END"
         else
-            # Die("Unhandled: GameState.DebugPrintPower: #{tags}")
+            Error "Unhandled: GameState.DebugPrintPower"
         end
     when "OnEntityChoices", "OnEntitiesChosen"
     when "DebugPrintEntityChoices", "DebugPrintEntitiesChosen"
@@ -103,15 +157,19 @@ def ParseGameState(type, tags)
         when /^(\S+)=(\S+)$/
             State.instance.setEntityTag "1", $1, $2
         when /^PlayerID=(\d+), PlayerName=(.*)$/
-            State.instance.setEntityTag $1, "PlayerName", $2
+            State.instance.players.each do |k, v|
+                if v.key? "PlayerID" and v["PlayerID"] == $1
+                    v["PlayerName"] = $2
+                end
+            end
         else
-            Die("Unhandled: GameState.DebugPrintGame: #{tags}")
+            Error "Unhandled: GameState.DebugPrintGame"
         end
     when "SendChoices"
     when "SendOption"
     when "DebugPrintOptions"
     else
-        Die("Unhandled GameState: #{type}")
+        Error "Unhandled GameState"
     end
 end
 
@@ -123,7 +181,7 @@ def ParsePowerTaskList(type, tags)
     when "EndCurrentTaskList"
     when "DoTaskListForCard"
     else
-        Die("Unhandled PowerTaskList: #{type}")
+        Error "Unhandled PowerTaskList: #{type}"
     end
 end
 
@@ -132,7 +190,17 @@ def ParsePowerProcessor(type, tags)
     when "DebugDump"
     when "DebugPrintPower"
     else
-        Die("Unhandled PowerProcessor: #{type}")
+        Error "Unhandled PowerProcessor: #{type}"
+    end
+end
+
+def ParseChoiceCardMgr(type, tags)
+    case type
+    when "WaitThenShowChoices"
+    when "DoesLocalChoiceMatchPacket"
+    when "WaitThenHideChoicesFromPacket"
+    else
+        Error "Unhandled ChoiceCardMgr: #{type}"
     end
 end
 
@@ -145,32 +213,27 @@ def Parse(line)
             ParsePowerProcessor $2, $3
         when "PowerProcessor"
             ParsePowerTaskList $2, $3
+        when "ChoiceCardMgr"
+            ParseChoiceCardMgr $2, $3
         else
-            Die("Unhadled log category: #{$1}")
+            Error "Unhadled log category: #{$1}"
         end
     else
-        Die("Unhandled log format")        
+        Error "Unhandled log format"        
     end
 end
 
-def Watch(f)
-    if true
-        f.each_line do |line|
-            yield line.strip
-        end
-    else
-        f.seek 0, IO::SEEK_END
-        loop do
-            line = f.read
-            unless line
-                sleep 0.1
-                next
-            end
-            p line
-            yield line.strip
-        end
-    end
-end
+# def Watch(f)
+#     f.seek 0, IO::SEEK_END
+#     loop do
+#         line = f.read
+#         unless line
+#             sleep 0.1
+#             next
+#         end
+#         yield line.strip
+#     end
+# end
 
 # $PROGRAM_NAME = "hearthstoned"
 
@@ -184,13 +247,14 @@ end
 
 # Dir.chdir "/"
 
-Watch File.open("/Applications/Hearthstone/Logs/Power.log", "r") do |line|
+# Watch File.open("/Applications/Hearthstone/Logs/Power.log", "r") do |line|
+$<.each_line do |line|
     State.instance.currentLine = line
     State.instance.currentLineNo += 1
     if State.instance.inGame
-        Parse line
+        Parse line.strip
     else
-        State.instance.reset(true) if line =~ /CREATE_GAME$/
+        State.instance.reset true if line =~ /GameState\.DebugPrintPower\(\) - CREATE_GAME$/
     end
 end
 
